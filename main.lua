a920f1073d0a7ae5bf9a4bf825222a010670c73b
Michael Riggs <secretcxman@gmail.com>
2016-12-22 11:57:01 -0600

    Adding the Windows files for a lua based 2d game engine.

---
+++ main.lua
@@ -0,0 +1,5 @@
+function love.draw()
+	love.graphics.print("Welcome to Improving Battleship!", 290, 20) 
+	love.graphics.print("Please enter your name", 315, 280)
+	love.graphics.rectangle("fill", 315, 300, 150, 10)
+end
