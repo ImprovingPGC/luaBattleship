f4ae252b66455394a8f0f8fa987820aa435ec52b
Michael Riggs <secretcxman@gmail.com>
2016-12-27 14:00:34 -0600

    Adding a library for basic gui components. See https://github.com/adonaac/yaoui

--- main.lua
+++ main.lua
@@ -1,5 +1,34 @@
+yui = require  'yaoui'
+
+function love.load()
+	yui.UI.registerEvents()
+	
+	local windowWidth = love.graphics.getWidth()
+	view = yui.View(0, 0, 0, 0, {
+		margin_top = 0,
+		margin_left = 0,
+		yui.Flow({
+			yui.HorizontalSpacing({ w = windowWidth/3}),
+			yui.Textinput({onEnter = function(self, text) print(text) end , w = windowWidth/3}),
+			yui.HorizontalSpacing({ w = windowWidth/3})
+		})
+	})
+end
+
+function love.update(dt)
+	yui.update({view})
+	view:update(dt)
+end
+
 function love.draw()
+	view:draw()
+end
+
+
+--[[	love.graphics.setColor(255, 255, 255, 255)
 	love.graphics.print("Welcome to Improving Battleship!", 290, 20) 
 	love.graphics.print("Please enter your name", 315, 280)
 	love.graphics.rectangle("fill", 315, 300, 150, 10)
-end
+	love.graphics.rectangle("fill", 365, 320, 50, 20)
+	love.graphics.setColor(0, 0, 0, 255)
+	love.graphics.print("Start!", 375, 325)]]
