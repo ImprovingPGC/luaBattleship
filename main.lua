5cfe5f0d26e95ba66f550aa181dd81d21c621497
Reed Harris <Reed.Harris@Improving.com>
2016-12-30 14:01:09 -0600

    Consistent indentation

--- main.lua
+++ main.lua
@@ -5,11 +5,11 @@ function love.update(dt)
 	local windowWidth = love.graphics.getWidth()
 	local windowHeight = love.graphics.getHeight()	
 
-    suit.layout:reset(windowWidth/3,0)
+	suit.layout:reset(windowWidth/3,0)
 	suit.Label("Welcome to Improving Battleship!", suit.layout:row(windowWidth/3, 30))
 	suit.layout:push(windowWidth/3, windowHeight/2)
 	suit.Label("Please enter your name", suit.layout:row(windowWidth/3, 30))
-    suit.Input(nameInput, suit.layout:row())
+	suit.Input(nameInput, suit.layout:row())
 	if nameInput.text ~= "" then
 		suit.Label("Hello, " .. nameInput.text .. "!", suit.layout:row())
 	end
@@ -17,22 +17,22 @@ function love.update(dt)
 	if suit.Button("Start!", suit.layout:row()).hit then
 		-- start game with the given name
 	end
-    suit.layout:row()
+	suit.layout:row()
 
-    if suit.Button("Close", suit.layout:row()).hit then
-        love.event.quit()
-    end
+	if suit.Button("Close", suit.layout:row()).hit then
+		love.event.quit()
+	end
 end
 
 function love.draw()
-    suit.draw()
+	suit.draw()
 end
 
 function love.textinput(t)
-    suit.textinput(t)
+	suit.textinput(t)
 end
 
 function love.keypressed(key)
 	love.keyboard.setKeyRepeat(true)
-    suit.keypressed(key)
+	suit.keypressed(key)
 end
